5fc403133e6b9ff35cffc5618eadbff3add9c9bb
Kevin <fruitwasp@users.noreply.github.com>
2019-10-25 15:02:55 +0200

    Ditch compiler pass

--- src/Email/Composer/ComposerIdentifiers.php
+++ src/Email/Composer/ComposerIdentifiers.php
@@ -5,8 +5,8 @@ namespace FH\MailerBundle\Email\Composer;
 
 final class ComposerIdentifiers
 {
-    public const TEMPLATED_EMAIL    = 'templated_email_composer';
-    public const EMAIL              = 'email_composer';
+    public const TEMPLATED_EMAIL    = 'templated_email';
+    public const EMAIL              = 'email';
 
     private function __construct()
     {
