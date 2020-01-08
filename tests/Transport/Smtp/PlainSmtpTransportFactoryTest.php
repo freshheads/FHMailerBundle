4752735315b94ccecc246f9702ab1fbb7311e21d
Misha Klomp <misha.klomp@freshheads.com>
2020-01-08 11:23:24 +0100

    test if dsn supported (or not)

--- tests/Transport/Smtp/PlainSmtpTransportFactoryTest.php
+++ tests/Transport/Smtp/PlainSmtpTransportFactoryTest.php
@@ -20,11 +20,35 @@ class PlainSmtpTransportFactoryTest extends TestCase
     }
 
     /**
-     * @covers \FH\Bundle\MailerBundle\Composer\ApplyEmailMessageOptions
+     * @covers \FH\Bundle\MailerBundle\Transport\Smtp\PlainSmtpTransportFactory
+     */
+    public function testSupported(): void
+    {
+        $dsn = new Dsn('plainsmtp', 'localhost');
+
+        $supports = $this->factory->supports($dsn);
+
+        $this->assertTrue($supports);
+    }
+
+    /**
+     * @covers \FH\Bundle\MailerBundle\Transport\Smtp\PlainSmtpTransportFactory
+     */
+    public function testUnsupported(): void
+    {
+        $dsn = new Dsn('smtp', 'localhost');
+
+        $supports = $this->factory->supports($dsn);
+
+        $this->assertFalse($supports);
+    }
+
+    /**
+     * @covers \FH\Bundle\MailerBundle\Transport\Smtp\PlainSmtpTransportFactory
      */
     public function testCreateTransport(): void
     {
-        $dsn = new Dsn('smtp', 'localhost');
+        $dsn = new Dsn('plainsmtp', 'localhost');
         $transport = $this->factory->create($dsn);
         /** @var SocketStream $stream */
         $stream = $transport->getStream();
@@ -36,11 +60,11 @@ class PlainSmtpTransportFactoryTest extends TestCase
     }
 
     /**
-     * @covers \FH\Bundle\MailerBundle\Composer\ApplyEmailMessageOptions
+     * @covers \FH\Bundle\MailerBundle\Transport\Smtp\PlainSmtpTransportFactory
      */
     public function testCreateTransportNoDefaultPort(): void
     {
-        $dsn = new Dsn('', 'localhost', null, null, 30);
+        $dsn = new Dsn('plainsmtp', 'localhost', null, null, 30);
         $transport = $this->factory->create($dsn);
         /** @var SocketStream $stream */
         $stream = $transport->getStream();
